81c38a15e28324c04d80c96b05d40702da1ea840
kk <705555262@qq.com>
2019-11-12 19:31:56 +0800

    新增日志数据模型

--- learntime/operation/models.py
+++ learntime/operation/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.db.models import DateTimeField
 
@@ -20,3 +21,21 @@ class StudentActivity(models.Model):
         verbose_name = "学生活动表"
         verbose_name_plural = verbose_name
         db_table = "student_activities"
+
+
+class Log(models.Model):
+    """操作日志"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             verbose_name="操作者", related_name="my_logs")
+    content = models.TextField(verbose_name="内容")
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True,
+                                      verbose_name='创建时间')
+
+    class Meta:
+        verbose_name = "日志"
+        verbose_name_plural = verbose_name
+        db_table = "log"
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.content
