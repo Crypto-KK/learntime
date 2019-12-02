9a5244088b3338b0fef316e4557830e304965004
kk <705555262@qq.com>
2019-12-02 21:50:22 +0800

    完成用户测试

--- learntime/users/tests/test_models.py
+++ learntime/users/tests/test_models.py
@@ -1,8 +1,12 @@
-import pytest
-from django.conf import settings
-
-pytestmark = pytest.mark.django_db
+from test_plus.test import TestCase
 
 
-def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
+class TestUser(TestCase):
+
+    def setUp(self) -> None:
+        self.user = self.make_user()
+        self.user.name = "user01"
+        self.user.save()
+
+    def test__str__(self):
+        self.assertEqual(self.user.__str__(), "user01")
--- learntime/users/tests/test_views.py
+++ learntime/users/tests/test_views.py
@@ -1,52 +1,33 @@
-import pytest
-from django.conf import settings
+
 from django.test import RequestFactory
+from test_plus import TestCase
 
-from learntime.users.views import UserRedirectView, UserUpdateView
-
-pytestmark = pytest.mark.django_db
-
-
-class TestUserUpdateView:
-    """
-    TODO:
-        extracting view initialization code as class-scoped fixture
-        would be great if only pytest-django supported non-function-scoped
-        fixture db access -- this is a work-in-progress for now:
-        https://github.com/pytest-dev/pytest-django/pull/258
-    """
-
-    def test_get_success_url(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
-        view = UserUpdateView()
-        request = request_factory.get("/fake-url/")
-        request.user = user
-
-        view.request = request
-
-        assert view.get_success_url == f"/users/{user.username}/"
-
-    def test_get_object(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
-        view = UserUpdateView()
-        request = request_factory.get("/fake-url/")
-        request.user = user
-
-        view.request = request
-
-        assert view.get_object() == user
+from learntime.users.views import AdminUpdateView
 
 
-class TestUserRedirectView:
-    def test_get_redirect_url(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
-        view = UserRedirectView()
-        request = request_factory.get("/fake-url")
-        request.user = user
+class BaseUserTestCase(TestCase):
 
-        view.request = request
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+        self.user = self.make_user()
 
-        assert view.get_redirect_url() == f"/users/{user.username}/"
+
+class TestUserUpdateView(BaseUserTestCase):
+
+    def setUp(self) -> None:
+        super(TestUserUpdateView, self).setUp()
+        self.view = AdminUpdateView()
+        request = self.factory.get('/fake')
+        request.user = self.user
+        from django.contrib.messages.storage.fallback import FallbackStorage
+        setattr(request, 'session', 'session')
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+        self.view.request = request
+
+
+    def test_get_success_url(self):
+        self.assertEqual(self.view.get_success_url(), '/users/admins/')
+
+    def test_get_obj(self):
+        self.assertEqual(self.view.get_object(), self.user)
