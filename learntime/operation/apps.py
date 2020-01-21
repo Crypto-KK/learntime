ac470031907bae9b2687800dfed3392e42e62780
kk <705555262@qq.com>
2020-01-21 13:26:13 +0800

    增加签退成功signals.py

--- learntime/operation/apps.py
+++ learntime/operation/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class OperationConfig(AppConfig):
     name = 'learntime.operation'
+
+    def ready(self):
+        try:
+            import learntime.operation.signals
+        except ImportError:
+            pass
---
+++ learntime/operation/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+from learntime.operation.models import StudentActivity
+from learntime.utils.helpers import add_credit
+
+@receiver(post_save, sender=StudentActivity)
+def sign_out_activity(sender, instance=None, created=False, **kwargs):
+    """
+    签退活动，增加学时
+    """
+    if not created and instance.status == 3:
+        # 当status状态更新为签退成功时，增加学时
+        credit = instance.credit
+        credit_type = instance.credit_type
+        # 增加学时
+        add_credit(settings.CREDIT_TYPE, instance.student_id, credit_type, credit)
