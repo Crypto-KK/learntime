b427c17521f147c5a4cfbde0f215be1fe2fc62f4
kk <705555262@qq.com>
2020-10-22 08:58:20 +0800

    refactor(activity): 精简活动代码

--- learntime/activity/urls.py
+++ learntime/activity/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 
-from learntime.activity import views
 
 app_name = "activities"
 urlpatterns = [
-    # 根据学院获取管理员地址
-    path("get-admins/", view=views.GetAdminsView.as_view(), name='get_admins')
 ]
