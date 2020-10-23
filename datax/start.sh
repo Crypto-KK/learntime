936d263868c1ff0b3072db23da6bb230876cb8f1
kk <705555262@qq.com>
2020-10-23 12:10:53 +0800

    feat(): 新增备份脚本

---
+++ datax/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python ~/datax/bin/datax.py ~/learntime/datax/academy.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/users_user.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/student_backend.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/student.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/student_credit_verify.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/log.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/grade.json&&
+python ~/datax/bin/datax.py ~/learntime/datax/django_session.json
